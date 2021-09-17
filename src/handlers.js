87f019db054a6a68cf1baa1247ff95e103889b73
Eyad Alsaba <80719317+EyadAlsaba@users.noreply.github.com>
2021-09-17 23:29:56 +0200

    Update handlers.js

--- src/handlers.js
+++ src/handlers.js
@@ -54,7 +54,7 @@ export function renderUserQuery () {
                 document.querySelector('.confirmedCases').textContent = `Confirmed Cases: ${Confirmed.toLocaleString()}`;
                 document.querySelector('.deathsCases').textContent = `Deaths Cases: ${Deaths.toLocaleString()}`;
                 document.querySelector('.lastUpdate').textContent = `Last updated: ${Date.slice(0,10)}`;
-                document.querySelector('.percentageMsg').innerHTML = `Based on the statistics, we provide you with a percentage numbers of deaths cases from the confirmed cases!<br> for <span><em style="color: red;">${Country},</em><br></span> the percentage is <em><span style="color: red;">${percentageMessage}%</span></em>`
+                document.querySelector('.percentageMsg').innerHTML = `Based on the statistics, we provide you with a percentage numbers of deaths cases from the confirmed cases!<br> for <span><em style="color: red;">${Country}</em><br></span> the percentage is <em><span style="color: red;">${percentageMessage}%</span></em>`
                 document.querySelector('.flag').src = flag;
 
                 if(errorContainer){
