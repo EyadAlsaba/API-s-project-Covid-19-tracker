b65098978249f3a6d224cae27f5e1e1bbcb11bf9
Eyad Alsaba <80719317+EyadAlsaba@users.noreply.github.com>
2022-01-15 11:05:08 +0100

    Update handlers.js

--- src/handlers.js
+++ src/handlers.js
@@ -26,7 +26,8 @@ export function renderUserQuery () {
                 }
 
             const url = `https://api.covid19api.com/total/dayone/country/${userQuery}`;
-            const urlCountry = `https://restcountries.eu/rest/v2/name/${userQuery}`;
+            // The API is not working anymore 
+            //const urlCountry = `https://restcountries.eu/rest/v2/name/${userQuery}`;
 
             if(isNaN(userQuery) === false || !userQuery) {
 
@@ -42,21 +43,21 @@ export function renderUserQuery () {
                 const { Country, Active, Confirmed, Deaths, Date } = lastIndex;   
     
                 const countryRequest = await fetchData(urlCountry);
-                const { flag, population } = countryRequest[0];
+                //const { flag, population } = countryRequest[0];
 
                 const percentageDeathsOfConfirmedCases = Deaths / Confirmed * 100;
                 const percentageMessage = percentageDeathsOfConfirmedCases.toFixed(2);
 
                 renderResultSection();
                 document.querySelector('.countryName').textContent = Country;
-                document.querySelector('.countryPopulation').textContent = `population: ${population.toLocaleString()}`;               
+                // document.querySelector('.countryPopulation').textContent = `population: ${population.toLocaleString()}`;               
                 document.querySelector('.activeCases').textContent = `Active cases: ${Active.toLocaleString()}`;
                 document.querySelector('.confirmedCases').textContent = `Confirmed Cases: ${Confirmed.toLocaleString()}`;
                 document.querySelector('.deathsCases').textContent = `Deaths Cases: ${Deaths.toLocaleString()}`;
                 document.querySelector('.lastUpdate').textContent = `Last updated: ${Date.slice(0,10)}`;
                 document.querySelector('.percentageMsg').innerHTML =
                  `Based on the statistics, we provide you with a percentage numbers of deaths cases from the confirmed cases!<br> for <span><em style="color: red;">${Country}</em><br></span> the percentage is <em><span style="color: red;">${percentageMessage}%</span></em>`
-                document.querySelector('.flag').src = flag;
+                // document.querySelector('.flag').src = flag;
 
                 if(errorContainer){
                     errorContainer.remove(); 
